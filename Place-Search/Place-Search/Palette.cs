b767381cf9336ab5981f477eb339e37449428f56
saintympheroane <Kwena.Mpheroane@boxfusion.io>
2024-08-16 18:37:53 +0200

    chore: add app colors

---
+++ Place-Search/Place-Search/Palette.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PlaceSearch
+{
+	public class Palette
+	{
+        public static readonly Color PrimaryColor = Color.FromHex("#ec027e");
+    }
+}
+
