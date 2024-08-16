b249fed6d63a52e820110f782612ce26fafe09bf
saintympheroane <Kwena.Mpheroane@boxfusion.io>
2024-08-16 18:36:26 +0200

    feat:add place details page

---
+++ Place-Search/Place-Search/Views/LocationDetailsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using PlaceSearch.Models;
+using PlaceSearch.ViewModels;
+using Xamarin.Forms;
+namespace PlaceSearch.Views
+{	
+	public partial class LocationDetailsPage : ContentPage
+	{	
+		public LocationDetailsPage (string placeId)
+		{
+			InitializeComponent ();
+            BindingContext = new LocationDetailsViewModel(ViewModelLocator.LocationService);
+            (BindingContext as LocationDetailsViewModel)?.LoadLocationDetailsAsync(placeId);
+        }
+	}
+}
+
