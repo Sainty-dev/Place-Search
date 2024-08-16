2c9c82eadc8910edee8c7aa6038e2fe4a49d642c
saintympheroane <Kwena.Mpheroane@boxfusion.io>
2024-08-16 03:25:46 +0200

    chore: add Place and LocationDetails Models

---
+++ Place-Search/Place-Search/Models/LocationDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PlaceSearch.Models
+{
+	public class LocationDetails
+	{
+        public string Name { get; set; }
+        public string FormattedAddress { get; set; }
+        public string Vicinity { get; set; }
+        public string Url { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int UtcOffset { get; set; }
+    }
+}
+
---
+++ Place-Search/Place-Search/Models/Place.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PlaceSearch.Models
+{
+	public class Place
+	{
+        public string PlaceId { get; set; }
+        public string Description { get; set; }
+        public string MainText { get; set; }
+        public string SecondaryText { get; set; }
+    }
+}
+
