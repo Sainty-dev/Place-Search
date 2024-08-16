cc944989a8287402495a28b7b25cca9a884f3dc8
Sainty-dev <82646950+Sainty-dev@users.noreply.github.com>
2024-08-16 19:50:02 +0200

    Chore:Update Constants.cs

--- Place-Search/Place-Search/Constants.cs
+++ Place-Search/Place-Search/Constants.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 namespace PlaceSearch
 {
-	public class Constants
-	{
+    public class Constants
+    {
         public const string BaseUrl = "https://staging.api.eos.kerridgecs.online/location/";
         public const string AuthUrl = "https://staging.identity.eos.kerridgecs.online/connect/token";
         public const string ApiKey = "b609f130-2d13-43d4-93df-f8ab9f1cafde";
