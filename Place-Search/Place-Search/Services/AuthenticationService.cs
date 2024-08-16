89a45f539d284cd89f10746dfda939a4263b9851
saintympheroane <Kwena.Mpheroane@boxfusion.io>
2024-08-16 18:38:30 +0200

    chore:update Auth service

--- Place-Search/Place-Search/Services/AuthenticationService.cs
+++ Place-Search/Place-Search/Services/AuthenticationService.cs
@@ -4,49 +4,43 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using PlaceSearch.Models;
 using Newtonsoft.Json;
+using RestSharp;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.Shapes;
 
 namespace PlaceSearch.Services
 {
-	public class AuthenticationService: IAuthenticationService
+    public class AuthenticationService : IAuthenticationService
     {
-        private readonly HttpClient _httpClient;
+        private readonly IRestClient _client;
         private readonly string _clientId;
         private readonly string _clientSecret;
-        private readonly string _tokenEndpoint;
-        private string _token;
 
-        public AuthenticationService(HttpClient httpClient, string clientId, string clientSecret, string tokenEndpoint)
+        public AuthenticationService(IRestClient client, string clientId, string clientSecret)
         {
-            _httpClient = httpClient;
+            _client = client;
             _clientId = clientId;
             _clientSecret = clientSecret;
-            _tokenEndpoint = tokenEndpoint;
         }
-
-        public async Task<string> GetTokenAsync()
+        /// <summary>
+        /// Get the access token
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> GetAccessTokenAsync()
         {
-            if (!string.IsNullOrEmpty(_token))
-            {
-                return _token;
-            }
+            var request = new RestRequest(Constants.AuthUrl, Method.Post);
+            request.AddParameter("grant_type", "client_credentials");
+            request.AddParameter("scope", "eos.common.fullaccess"); 
+            request.AddParameter("client_id", _clientId);
+            request.AddParameter("client_secret", _clientSecret);
 
-            var clientCredentials = new Dictionary<string, string>
-            {
-                { "grant_type", "client_credentials" },
-                { "client_id", _clientId },
-                { "client_secret", _clientSecret }
-            };
-
-            var requestContent = new FormUrlEncodedContent(clientCredentials);
-            var response = await _httpClient.PostAsync(_tokenEndpoint, requestContent);
-            response.EnsureSuccessStatusCode();
-
-            var content = await response.Content.ReadAsStringAsync();
-            var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(content);
-            _token = tokenResponse.AccessToken;
-
-            return _token;
+            var response = await _client.ExecuteAsync<TokenResponse>(request);
+            return response.IsSuccessful ? response.Data.AccessToken: null;
         }
+
+
+
+        
     }
 }
 
