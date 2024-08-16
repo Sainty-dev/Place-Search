ecb92baddce26e9ff2a43b2a22598c2711495e2a
saintympheroane <Kwena.Mpheroane@boxfusion.io>
2024-08-16 18:36:03 +0200

    feat:add main list page

---
+++ Place-Search/Place-Search/Views/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PlaceSearch.Views
+{	
+	public partial class MainPage : ContentPage
+	{	
+		public MainPage ()
+		{
+			InitializeComponent ();
+            BindingContext = ViewModelLocator.MainViewModel;
+        }
+        private async void OnPlaceSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection == null || e.CurrentSelection.Count == 0)
+                return;
+
+            if (e.CurrentSelection[0] is Models.PlaceData place)
+            {
+                // Navigate to location details page
+                await Navigation.PushAsync(new LocationDetailsPage(place.PlaceId));
+
+                // Clear the selection
+                if (sender is CollectionView collectionView)
+                {
+                    collectionView.SelectedItem = null;
+                }
+            }
+        }
+
+    }
+}
+
