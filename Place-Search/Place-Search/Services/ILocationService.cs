9c161bea897710d38539b6572df58ca01bbf9aaf
saintympheroane <Kwena.Mpheroane@boxfusion.io>
2024-08-16 03:40:36 +0200

    chore: add ILocation and Location Service

---
+++ Place-Search/Place-Search/Services/ILocationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PlaceSearch.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PlaceSearch.Services
+{
+	public interface ILocationService
+	{
+        Task<IEnumerable<Place>> SearchPlacesAsync(string query);
+        Task<LocationDetails> GetLocationDetailsAsync(string placeId);
+    }
+}
+
---
+++ Place-Search/Place-Search/Services/LocationService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PlaceSearch.Models;
+
+namespace PlaceSearch.Services
+{
+	public class LocationService: ILocationService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IAuthenticationService _authService;
+
+        public LocationService(HttpClient httpClient, IAuthenticationService authService)
+        {
+            _httpClient = httpClient;
+            _authService = authService;
+        }
+
+        public async Task<IEnumerable<Place>> SearchPlacesAsync(string query)
+        {
+            var token = await _authService.GetTokenAsync();
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.GetAsync($"/api/v1/locations/places?query={query}");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var places = JsonConvert.DeserializeObject<IEnumerable<Place>>(content);
+            return places;
+        }
+
+        public async Task<LocationDetails> GetLocationDetailsAsync(string placeId)
+        {
+            var token = await _authService.GetTokenAsync();
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.GetAsync($"/api/v1/locations/places/{placeId}");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var details = JsonConvert.DeserializeObject<LocationDetails>(content);
+            return details;
+        }
+    }
+}
+
